db419fbd62af43357f3c6a215810d13ea2a8183e
Amanda D'Argenio <dargenioa14@gmail.com>
2021-01-17 13:39:01 -0500

    created navbar, installed npm packages, started components pages and utils folder

---
+++ employee-directory/src/components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Employee Directory
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/search"
+              className={
+                window.location.pathname === "/search"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
