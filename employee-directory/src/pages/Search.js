0b753f22bc04628c234e13ba11df578dbea6721d
Amanda D'Argenio <dargenioa14@gmail.com>
2021-01-18 15:56:45 -0500

    rendered grid using row and col components

--- employee-directory/src/pages/Search.js
+++ employee-directory/src/pages/Search.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-// import Container from "../components/Container/Container";
+import Container from "../components/Container/Container";
 import SearchForm from "../components/SearchForm/SearchForm";
 // import SearchResults from "../components/SearchResults/SearchResults";
-// import Row from "../components/Row/Row";
-// import Col from "../components/Col/Col";
+import Row from "../components/Row/Row";
+import Col from "../components/Col/Col";
 import API from "../utils/API";
 
 class Search extends Component {
@@ -60,11 +60,34 @@ class Search extends Component {
     return (
       <div>
         <SearchForm />,
-        <ul>
-          {this.state.employees.map((employee) => {
-            return <li key={employee.email}>{employee.name.first}</li>;
-          })}
-        </ul>
+        <Container>
+          <Row>
+            <Col size="md-12">
+              {this.state.employees.map((employee) => {
+                return (
+                  <Row>
+                    <Col size="md-2">
+                      <img
+                        alt="portrait"
+                        src={employee.picture.medium}
+                      ></img>
+                    </Col>
+                    <Col size="md-3">
+                      <p key={employee.id.value}>
+                        {employee.name.first} {employee.name.last}
+                      </p>
+                    </Col>
+                    <Col size="md-3">
+                      <p key={employee.id.value}>
+                        {employee.email}
+                      </p>
+                    </Col>
+                  </Row>
+                );
+              })}
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
