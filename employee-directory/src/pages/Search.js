72e60c94b07a5cd2d49ad02dadb892a4b94ee441
Amanda D'Argenio <dargenioa14@gmail.com>
2021-01-22 17:32:42 -0500

    completed search capability of being able to search by any of the rendered results for the employee and changing the state of search and searchResults

--- employee-directory/src/pages/Search.js
+++ employee-directory/src/pages/Search.js
@@ -5,7 +5,6 @@ import SearchForm from "../components/SearchForm/SearchForm";
 import Row from "../components/Row/Row";
 import Col from "../components/Col/Col";
 import API from "../utils/API";
-import "@babel/polyfill";
 
 class Search extends Component {
   state = {
@@ -30,7 +29,8 @@ class Search extends Component {
       .then((res) =>
         this.setState({
           employees: res.data.results,
-          search: res.data.results,
+          searchResults: res.data.results,
+          
         })
       )
       .catch((err) => console.log(err));
@@ -40,19 +40,60 @@ class Search extends Component {
     this.setState({
       search: event.target.value,
     });
-  };
+  }
+  //   .then()
+  //     const results = this.state.employees.filter(employee => {
+  //       let searchEmployee = Object.values(employee)
+  //       .join("")
+  //       .toLowerCase();
+  //       return searchEmployee.indexOf(this.state.search.toLowerCase() !== -1);
+  //     })
+  //     .then(
+  //       this.setState({
+  //         searchResults: results
+  //       })
+  //     )
+  // };
 
   handleSubmitForm = (event) => {
     event.preventDefault();
-    const results = this.state.employees.filter(employee => employee.contains(this.state.search))
-      .then(
-        this.setState({
-          searchResults: results,
-        })
-      )
-      .catch((err) => {
-        console.log(err);
-      })
+    const results = this.state.employees.filter(employee => {
+      return employee.name.first.toLowerCase().includes(this.state.search) ||
+      employee.name.last.toLowerCase().includes(this.state.search) ||
+      employee.email.toLowerCase().includes(this.state.search) ||
+      employee.location.city.toLowerCase().includes(this.state.search) ||
+      employee.location.state.toLowerCase().includes(this.state.search) ||
+      employee.cell.toLowerCase().includes(this.state.search);
+    });
+    console.log(results);
+    this.setState({
+      searchResults: results
+    })
+    
+         // const results = {
+    //   employees: Object.values(this.state.employees)
+    // };
+    // //
+    // results.filter((employee) => employee.includes(this.state.search))
+    // .then(
+      // this.setState({
+      //       searchResults: results,
+      //     });
+    // .catch((err) => {
+    //     console.log(err);
+    //   })
+    
+    // console.log(results);
+    
+    // this.state.employees.filter(employee => employee.contains(this.state.search))
+      // .then(
+      //   this.setState({
+      //     searchResults: results,
+      //   })
+      // )
+      // .catch((err) => {
+      //   console.log(err);
+      // })
   };
 
   render() {
@@ -67,29 +108,29 @@ class Search extends Component {
         <Container>
           <Row>
             <Col size="md-12">
-              {this.state.employees.map((employee) => {
+              {this.state.searchResults.map((employee) => {
                 return (
                   <Row>
                     <Col size="md-2">
                       <img
                         alt="portrait"
                         src={employee.picture.medium}
-                        key={employee.id.value}
+                        key={employee.login.uuid}
                       ></img>
                     </Col>
                     <Col size="md-2">
-                      <p key={employee.id.value}>
+                      <p key={employee.login.uuid}>
                         {employee.name.first} {employee.name.last}
                       </p>
                     </Col>
                     <Col size="md-3">
-                      <p key={employee.id.value}>{employee.email}</p>
+                      <p key={employee.login.uuid}>{employee.email}</p>
                     </Col>
                     <Col size="md-2">
-                      <p key={employee.id.value}>{employee.cell}</p>
+                      <p key={employee.login.uuid}>{employee.cell}</p>
                     </Col>
                     <Col size="md-2">
-                      <p key={employee.id.value}>
+                      <p key={employee.login.uuid}>
                         {employee.location.city}, {employee.location.state}
                       </p>
                     </Col>
